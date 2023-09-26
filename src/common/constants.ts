429dd282f6494cb2b755350b1745ad7b88a2b27a
Joel <lavotivinay@gmail.com>
2023-09-26 08:47:40 +0530

    revert mogno url

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 export const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
 
-// export const DB_URL = `mongodb+srv://joel123:${process.env.MONGODB_PASSWORD}@cluster0.ullfa.mongodb.net/?retryWrites=true&w=majority`;
+export const DB_URL = `mongodb+srv://joel123:${process.env.MONGODB_PASSWORD}@cluster0.ullfa.mongodb.net/?retryWrites=true&w=majority`;
 
-export const DB_URL = 'mongodb://127.0.0.1:27017/compare-course';
+// export const DB_URL = 'mongodb://127.0.0.1:27017/compare-course';
